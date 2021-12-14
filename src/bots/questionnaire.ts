915deb92bda2a700f574f1f37034c17dc444613f
KowerKoint <kowerkoint@gmail.com>
2021-12-14 12:38:10 +0900

    slash commandやりかけ

--- src/bots/questionnaire.ts
+++ src/bots/questionnaire.ts
@@ -1,25 +1,36 @@
 import process from "process";
 import { BotRunner} from "../interfaces"
-import { Client, Intents, Message } from "discord.js";
+import { ApplicationCommandData, Client, Intents, Interaction, Message } from "discord.js";
 
 require('dotenv').config();
 
 export class QuestionnaireBot implements BotRunner {
     private token = process.env.QUESTIONNAIRE_BOT_TOKEN;
+    private client: Client | null = null;
 
     awake() {
         console.log('awaked');
-        const client = new Client({
+        this.client = new Client({
             intents: [
                 Intents.FLAGS.GUILDS,
                 Intents.FLAGS.GUILD_MESSAGES,
             ]
         });
         
-        client.once('ready', this.on_ready);
-        client.on('messageCreate', this.on_message);
+        this.client.once('ready', this.on_ready);
+        this.client.on('messageCreate', this.on_message);
+        this.client.on("interactionCreate", this.on_interaction);
 
-        client.login(this.token);
+        this.client.login(this.token);
+    }
+    
+    async on_interaction(interaction: Interaction) {
+        console.log(interaction);
+        if(interaction.isCommand()) {
+            if(interaction.commandName === 'ping') {
+                await interaction.reply('Pong!');
+            }
+        }
     }
     
     async on_message(message: Message) {
@@ -30,6 +41,15 @@ export class QuestionnaireBot implements BotRunner {
     }
     
     async on_ready() {
+        const data: ApplicationCommandData[] = [
+            {
+                name: "ping",
+                description: "Replies with Pong!"
+            }
+        ];
+        this.client?.application?.commands.set(data, '695951623964590090').then(c => {
+            console.log(c);
+        })
         console.log('QuestionnaireBot is ready!');
     }
 }
